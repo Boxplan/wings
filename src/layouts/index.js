3056980bdac839133f30d8055647b3d9cbb5c11c
Jxp <catchjim79@gmail.com>
2018-06-05 13:03:38 +0530

    chat window auto open disabled

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -201,7 +201,7 @@ class Template extends React.Component {
                 <div
                     data-start="What Event are you planning ?"
                     data-greet="Wings Banquet Hall"
-                    data-float="1"
+                    data-float=""
                     data-vendorMail=""
                     data-light="#EEF2F5"
                     data-primary="#39332f"
